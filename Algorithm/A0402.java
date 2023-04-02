c30b44a9594f23876bb9a555f51d96fd6a9c6a7a
DawnChoi <totosie91@gmail.com>
2023-04-02 19:07:59 +0900

    0402TEST

---
+++ Algorithm/A0402.java
@@ -0,0 +1,10 @@
+
+// Hello World (2557번)
+public class A0402 {
+    public static void main(String[] args) {
+  
+        System.out.println("Hello World!"); //문자열 출력
+        
+    }//main
+ 
+}//class
