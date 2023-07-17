a5c138dbcf4843c8cc40bdf0c473549a5b559d9b
SangboIT <129030075+SangboIT@users.noreply.github.com>
2023-07-17 15:40:53 +0900

    0717C#
    0717C#

---
+++ C#/ConsoleApp2/ConsoleApp2/Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp2
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            //Console.WriteLine("Hello World");
+            //Console.WriteLine(30);
+            //Console.WriteLine(30.1);
+            //Console.WriteLine(30.2f);
+            //Console.WriteLine(true);
+            //Console.Write("Hello World");
+            Console.Write("p");
+        }
+    }
+}
